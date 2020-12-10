8181398e1053743fae132a4bf4d575134ce33ca7
Noel Schenk <schenknoel@gmail.com>
2020-12-10 05:35:41 +0100

    added ai stuff, atm there seems to be an issue with the layers or optimizer

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { h, Component, render } from 'preact';
 import htm from 'htm';
-import { Subject } from 'rxjs';
-import { Template } from 'webpack';
+import { AsyncSubject, Observable, Subject } from 'rxjs';
 import clone from 'clone';
+import * as tf from '@tensorflow/tfjs';
+import { isTaggedTemplateExpression } from 'typescript';
 const html = htm.bind(h);
 
 class AusDieMausState {
@@ -19,6 +20,7 @@ class AusDieMausState {
 
 class AusDieMausBoard extends Component {
     private _ausDieMausState: AusDieMausState;
+
     public set ausDieMausState(state: AusDieMausState) {
         this._ausDieMausState = state;
         this.setState({ausDieMausState: state});
@@ -62,11 +64,14 @@ class RenderGame {
     }
 }
 
+type StateToReaction = Array<{state: AusDieMausState; reaction: AusDieMausState}>;
+
 class PlayGame {
     public activeState = new AusDieMausState();
     public currentPlayer = false; //true === player 1 ... false === player 2
-    public stateToReactionP1 = new Array<{state: AusDieMausState; reaction: AusDieMausState}>();
-    public stateToReactionP2 = new Array<{state: AusDieMausState; reaction: AusDieMausState}>();
+    
+    public stateToReactionP1: StateToReaction = new Array<{state: AusDieMausState; reaction: AusDieMausState}>();
+    public stateToReactionP2: StateToReaction = new Array<{state: AusDieMausState; reaction: AusDieMausState}>();
 
     public renderGame = new RenderGame();
     
@@ -74,11 +79,21 @@ class PlayGame {
         this.renderGame.render();
     }
 
-    public async training() {
+    public async training(ai?: (state: AusDieMausState) => void) {
         const helper = new Helper();
         while (!helper.checkIfFinished(this.activeState)) {
-            console.log('new move');
-            const newState = helper.getValidMove(this.activeState);
+            this.currentPlayer = !this.currentPlayer;
+            // console.log(this.currentPlayer === true ? 'Player 1' : 'Player 2');
+
+            let newState: AusDieMausState;
+
+            if (ai) {
+                await ai(this.activeState);
+                newState = helper.getValidMove(this.activeState);
+            } else {
+                newState = helper.getValidMove(this.activeState);
+            }
+
             const newStateToReaction = {state: clone(this.activeState), reaction: clone(newState)};
 
             if (this.currentPlayer) {
@@ -86,25 +101,26 @@ class PlayGame {
             } else {
                 this.stateToReactionP2.push(newStateToReaction);
             }
-            this.currentPlayer = !this.currentPlayer;
             this.activeState = newState;
             
             this.renderGame.updateAusDieMausState.next(newState);
             
             await new Promise(resolve => setTimeout(resolve, 10));
         }
+        console.log(this.currentPlayer ? 'Player 2 Wins!' : 'Player 1 Wins!');
+        return this.currentPlayer ? this.stateToReactionP2 : this.stateToReactionP1;
     }
 }
 
 class Helper {
-    public maxStates = [3,5,6,6,5,3];
+    public static maxStates = [3,5,6,6,5,3];
     
     constructor() {}
     
     checkIfFinished(state: AusDieMausState) {
         let isFinished = true;
         state.rows.forEach((row, i) => {
-            if (row < this.maxStates[i]) {
+            if (row < Helper.maxStates[i]) {
                 isFinished = false;
             }
         });
@@ -139,7 +155,7 @@ class Helper {
     }
     
     getMaxIncrease(row: number, state: AusDieMausState) {
-        return this.maxStates[row] - state.rows[row];
+        return Helper.maxStates[row] - state.rows[row];
     }
     
     checkIfMoveValid(state: AusDieMausState, lastState: AusDieMausState) {
@@ -151,7 +167,7 @@ class Helper {
     
     checkMaxs(vals: Array<number>) {
         vals.forEach((val, i) => {
-            if (!(val <= this.maxStates[i])) {
+            if (!(val <= Helper.maxStates[i])) {
                 return false;
             }
         });
@@ -159,11 +175,92 @@ class Helper {
     }
 }
 
+class AusDieMausAI {
+    private learningRate = 0.1;
+    
+    private model = tf.sequential();
+
+    private trainingData = new Array<StateToReaction>();
+
+    
+    constructor() {
+        this.model.add(tf.layers.dense({units: 6, inputShape: [6], activation: 'sigmoid'}));
+        this.model.add(tf.layers.dense({units: 12, activation: 'sigmoid'}));
+        this.model.add(tf.layers.dense({units: 18, activation: 'sigmoid'}));
+        this.model.add(tf.layers.dense({units: 12, activation: 'sigmoid'}));
+        this.model.add(tf.layers.dense({units: 6, activation: 'sigmoid'}));
+
+        this.model.compile({optimizer: tf.train.adam(this.learningRate), loss: 'meanSquaredError'});
+    }
+
+    addData(stateToReaction: StateToReaction) {
+        this.trainingData.push(stateToReaction);
+    }
+
+    async train() {
+        let trainingData = new Array<{state: AusDieMausState;reaction: AusDieMausState;}>();
+
+        for (let stateToReaction of this.trainingData) for (let s2r of stateToReaction) trainingData.push(s2r);
+
+        for (let td of trainingData) {
+            const normalizedState = td.state.rows.map((row, i) => this.normalizeData(row, Helper.maxStates[i]));
+            const normalizedReaction = td.reaction.rows.map((row, i) => this.normalizeData(row, Helper.maxStates[i]));
+
+            const stateTensor = tf.tensor(normalizedState, [1, 6]);
+            const reactionTensor = tf.tensor(normalizedReaction, [1, 6]);
+            
+            console.log(stateTensor, 'stateTensor');
+
+            await this.model.fit(
+                stateTensor,
+                reactionTensor,
+                {
+                    epochs: 1,
+                    batchSize: 1
+                }
+            );
+        }
+    }
+
+    classify(state: AusDieMausState) {
+        const inputData = tf.tensor(state.rows, [1, 6]);
+        const prediction: Array<number> = Array.from((this.model.predict(inputData) as tf.Tensor).dataSync());
+        const revertNormalizedPrediction = prediction.map((row, i) => this.revertNormalizeData(row, Helper.maxStates[i]));
+        console.log(revertNormalizedPrediction, 'prediction');
+    }
+
+    private normalizeData(val:number, max:number) {
+        return val/max;
+    }
+    private revertNormalizeData(val:number, max:number) {
+        return val*max;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+    console.log(tf);
+    let iteration:number = 0;
+
+    const ausDieMausAI = new AusDieMausAI();  
     const loadTraining = async () => {
         const newGame = new PlayGame();
-        await newGame.training();
-        console.log(newGame.stateToReactionP1, newGame.stateToReactionP2);
+        let winData: StateToReaction; 
+
+        console.log(`Iteration: ${iteration}`)
+
+        if (iteration === 200) {
+            await ausDieMausAI.train();
+        }
+
+        if (iteration >= 200) {
+            winData = await newGame.training((state: AusDieMausState) => ausDieMausAI.classify(state));
+        } else {
+            winData = await newGame.training();
+        }
+
+        ausDieMausAI.addData(winData);
+
+        iteration++;
         loadTraining();
     };
     loadTraining();
