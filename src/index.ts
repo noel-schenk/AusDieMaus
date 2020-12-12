ddeef49cd67500035a7bd5bb7ae1c4bfe55c1bf5
Noel Schenk <schenknoel@gmail.com>
2020-12-12 23:19:25 +0100

    moved to statistics

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { h, Component, render } from 'preact';
 import htm from 'htm';
 import { AsyncSubject, Observable, Subject } from 'rxjs';
 import clone from 'clone';
-import * as tf from '@tensorflow/tfjs';
-import { isTaggedTemplateExpression } from 'typescript';
+import PouchDB from 'pouchdb';
+import chartjs from 'chart.js';
 const html = htm.bind(h);
 
 class AusDieMausState {
@@ -15,6 +15,9 @@ class AusDieMausState {
         this.rows[3] = 0; //max 6
         this.rows[4] = 0; //max 5
         this.rows[5] = 0; //max 3
+    }
+    getStateID() {
+        return this.rows.join('');
     }
 }
 
@@ -33,7 +36,7 @@ class AusDieMausBoard extends Component {
         this.ausDieMausState = new AusDieMausState();
     }
     render({updateAusDieMausState}: any, {ausDieMausState = this.ausDieMausState}: {ausDieMausState: AusDieMausState}) {
-        (updateAusDieMausState as Subject<AusDieMausState>).subscribe((ausDieMausState: AusDieMausState) => {
+        (updateAusDieMausState as Subject<AusDieMausState>).subscribe(ausDieMausState => {
             this.ausDieMausState = ausDieMausState;
         });
         const htmFor = (times: number, cb: () => preact.VNode<any> | preact.VNode<any>[]) => {
@@ -57,29 +60,105 @@ class AusDieMausBoard extends Component {
     }
 }
 
-class RenderGame {
-    public updateAusDieMausState = new Subject<AusDieMausState>();
-    render() {
-        render(html`<${AusDieMausBoard} updateAusDieMausState=${this.updateAusDieMausState} />`, document.body);
+class PredictionMaker extends Component{
+    predictionRequest = '';
+    ausDieMausStatisticsSubject: Subject<AusDieMausStatistics>;
+    ausDieMausStatistics: AusDieMausStatistics;
+
+    componentDidMount() {
+        this.ausDieMausStatisticsSubject.subscribe(ausDieMausStatistics => {
+            this.ausDieMausStatistics = ausDieMausStatistics;
+        });
+    }
+
+    getPrediction() {
+        const ausDieMausState = new AusDieMausState();
+        ausDieMausState.rows = Array.from(String(this.predictionRequest), Number);
+
+        if (this.ausDieMausStatistics) {
+            this.ausDieMausStatistics.getData(ausDieMausState).then(ausDieMausState => {
+                console.log(`Try: ${(ausDieMausState as AusDieMausState)?.rows}`);
+                debugger;
+            });
+        } else {
+            console.info('NO Statistics FOUND');
+        }
+        
+    }
+
+    changePredictionRequest(e: InputEvent) {
+        this.predictionRequest = (e.target as HTMLInputElement).value;
+    }
+
+    render({ausDieMausStatistics}: any) {
+        this.ausDieMausStatisticsSubject = ausDieMausStatistics;
+
+        return html`
+            <input onInput=${(e: InputEvent) => this.changePredictionRequest(e)} />
+            <button onClick=${() => this.getPrediction()} />
+        `;
     }
 }
 
-type StateToReaction = Array<{state: AusDieMausState; reaction: AusDieMausState}>;
+class Chart extends Component {
+    data = new Array(30).fill(0);
+
+    updateChart: Subject<boolean>;
+
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+        this.updateChart.subscribe(async win => {
+            this.data.shift();
+            const currentWins = this.data.slice(-1)[0];
+            this.data.push(win ? currentWins + 1 : currentWins - 1);
+            this.data = this.data.map(data => data - this.data[0]);
+        });
+    }
+
+    render({updateChart}: any) {
+        this.updateChart = updateChart as Subject<boolean>;
+        return html`
+            <div class='chart'>
+            ${this.data.map(data => html`
+                <div class='chartData' style='height:${data}px'>
+                    
+                </div>
+            `)}
+            </div>
+        `;
+    }
+}
+
+class RenderGame {
+    public updateAusDieMausState = new Subject<AusDieMausState>();
+    public updateChart = new Subject<boolean>();
+    public ausDieMausStatistics = new Subject<AusDieMausStatistics>();
+    render() {
+        render(html`<${AusDieMausBoard} updateAusDieMausState=${this.updateAusDieMausState} /><${Chart} updateChart=${this.updateChart} /><${PredictionMaker} ausDieMausStatistics=${this.ausDieMausStatistics} />`, document.body);
+    }
+}
+
+type StateToReaction = {state: AusDieMausState; reaction: AusDieMausState};
+type StateToReactions = Array<StateToReaction>;
 
 class PlayGame {
     public activeState = new AusDieMausState();
     public currentPlayer = false; //true === player 1 ... false === player 2
     
-    public stateToReactionP1: StateToReaction = new Array<{state: AusDieMausState; reaction: AusDieMausState}>();
-    public stateToReactionP2: StateToReaction = new Array<{state: AusDieMausState; reaction: AusDieMausState}>();
+    public stateToReactionP1: StateToReactions = new Array<StateToReaction>();
+    public stateToReactionP2: StateToReactions = new Array<StateToReaction>();
 
-    public renderGame = new RenderGame();
+    public renderGame: RenderGame;
     
-    constructor() {
+    constructor(renderGame: RenderGame) {
+        this.renderGame = renderGame;
         this.renderGame.render();
     }
 
-    public async training(ai?: (state: AusDieMausState) => void) {
+    public async training(ai?: (state: AusDieMausState) => Promise<false | AusDieMausState>) {
         const helper = new Helper();
         while (!helper.checkIfFinished(this.activeState)) {
             this.currentPlayer = !this.currentPlayer;
@@ -87,9 +166,10 @@ class PlayGame {
 
             let newState: AusDieMausState;
 
-            if (ai) {
-                await ai(this.activeState);
-                newState = helper.getValidMove(this.activeState);
+            if (ai && this.currentPlayer) {
+                const aiRes = await ai(this.activeState);
+                console.log(aiRes, 'aiRes');
+                newState = aiRes ? aiRes : helper.getValidMove(this.activeState);
             } else {
                 newState = helper.getValidMove(this.activeState);
             }
@@ -108,7 +188,12 @@ class PlayGame {
             await new Promise(resolve => setTimeout(resolve, 10));
         }
         console.log(this.currentPlayer ? 'Player 2 Wins!' : 'Player 1 Wins!');
-        return this.currentPlayer ? this.stateToReactionP2 : this.stateToReactionP1;
+        const win = this.currentPlayer ? this.stateToReactionP2 : this.stateToReactionP1;
+        const loose = !this.currentPlayer ? this.stateToReactionP2 : this.stateToReactionP1
+
+        this.renderGame.updateChart.next(!this.currentPlayer);
+
+        return {win, loose};
     }
 }
 
@@ -175,92 +260,93 @@ class Helper {
     }
 }
 
-class AusDieMausAI {
-    private learningRate = 0.1;
-    
-    private model = tf.sequential();
+type Reaction = {winRate: number; dataAmount: number; rows: Array<number>};
+type Reactions = Map<string, Reaction>;
 
-    private trainingData = new Array<StateToReaction>();
+class AusDieMausStatistics {
 
-    
-    constructor() {
-        this.model.add(tf.layers.dense({units: 6, inputShape: [6], activation: 'sigmoid'}));
-        this.model.add(tf.layers.dense({units: 12, activation: 'sigmoid'}));
-        this.model.add(tf.layers.dense({units: 18, activation: 'sigmoid'}));
-        this.model.add(tf.layers.dense({units: 12, activation: 'sigmoid'}));
-        this.model.add(tf.layers.dense({units: 6, activation: 'sigmoid'}));
+    db = new PouchDB('AusDieMausAI');
 
-        this.model.compile({optimizer: tf.train.adam(this.learningRate), loss: 'meanSquaredError'});
-    }
+    constructor() {}
 
-    addData(stateToReaction: StateToReaction) {
-        this.trainingData.push(stateToReaction);
-    }
-
-    async train() {
-        let trainingData = new Array<{state: AusDieMausState;reaction: AusDieMausState;}>();
-
-        for (let stateToReaction of this.trainingData) for (let s2r of stateToReaction) trainingData.push(s2r);
-
-        for (let td of trainingData) {
-            const normalizedState = td.state.rows.map((row, i) => this.normalizeData(row, Helper.maxStates[i]));
-            const normalizedReaction = td.reaction.rows.map((row, i) => this.normalizeData(row, Helper.maxStates[i]));
-
-            const stateTensor = tf.tensor(normalizedState, [1, 6]);
-            const reactionTensor = tf.tensor(normalizedReaction, [1, 6]);
-            
-            console.log(stateTensor, 'stateTensor');
-
-            await this.model.fit(
-                stateTensor,
-                reactionTensor,
-                {
-                    epochs: 1,
-                    batchSize: 1
-                }
-            );
+    async getData(state: AusDieMausState) {
+        const stateID = state.getStateID();
+        const docData: any = await this.db.get(stateID).catch(() => {
+            return null;
+        }).then(docData => {
+            return docData;
+        })
+        const reactions: Reactions = docData?.reactions;
+        if (reactions) {
+            const reaction = Array.from(reactions.values()).reduce((p, c) => (p.winRate > c.winRate) ? p : c);
+            if (reaction) {
+                const newAusDieMausState = new AusDieMausState();
+                newAusDieMausState.rows = reaction.rows;
+                return newAusDieMausState;
+            }
         }
+        return false;
     }
 
-    classify(state: AusDieMausState) {
-        const inputData = tf.tensor(state.rows, [1, 6]);
-        const prediction: Array<number> = Array.from((this.model.predict(inputData) as tf.Tensor).dataSync());
-        const revertNormalizedPrediction = prediction.map((row, i) => this.revertNormalizeData(row, Helper.maxStates[i]));
-        console.log(revertNormalizedPrediction, 'prediction');
+    async addData(stateToReactions: StateToReactions, win: boolean) {
+        const change = win ? 1 : 0;
+        stateToReactions.forEach(stateToReaction => {
+            this.addDataToDb(stateToReaction, change);
+        });
     }
 
-    private normalizeData(val:number, max:number) {
-        return val/max;
-    }
-    private revertNormalizeData(val:number, max:number) {
-        return val*max;
+    async addDataToDb(stateToReaction: StateToReaction, change: number) { // change = 1 for win 0 for loose
+        const stateID = stateToReaction.state.getStateID();
+        const docData: any = await this.db.get(stateID).catch(() => {
+            return null;
+        }).then(docData => {
+            return docData;
+        })
+        const reactions: Reactions = docData?.reactions;
+        const reactionStateID = stateToReaction.reaction.getStateID();
+        if (reactions) { // if there are already reactions for this state recorded
+            const dbProposedState = reactions.get(reactionStateID);
+            if (dbProposedState) { // if there is already this reaction for a win or loose recorded
+                const winRate = dbProposedState.winRate;
+                const dataAmount: number = dbProposedState.dataAmount;
+                dbProposedState.winRate = (((winRate * dataAmount) + change) / (dataAmount + 1));
+                dbProposedState.dataAmount = dataAmount + 1;
+                reactions.set(reactionStateID, dbProposedState);
+                await this.db.put({_id: stateID, reactions, _rev: docData?._rev }, {force: true});
+            } else {
+                reactions.set(reactionStateID, { winRate: change, dataAmount: 1, rows: stateToReaction.reaction.rows });
+                await this.db.put({_id: stateID, reactions, _rev: docData?._rev }, {force: true});
+            }
+        } else {
+            const newReactions: Reactions = new Map();
+            newReactions.set(reactionStateID, { winRate: change, dataAmount: 1, rows: stateToReaction.reaction.rows });
+            await this.db.put({_id: stateID, reactions: newReactions, _rev: docData?._rev }, {force: true});
+        }
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log(tf);
     let iteration:number = 0;
 
-    const ausDieMausAI = new AusDieMausAI();  
+    const ausDieMausStatistics = new AusDieMausStatistics();
+    const renderGame = new RenderGame();
+    renderGame.render();
+    renderGame.ausDieMausStatistics.next(ausDieMausStatistics);
+
     const loadTraining = async () => {
-        const newGame = new PlayGame();
-        let winData: StateToReaction; 
+        const newGame = new PlayGame(renderGame);
 
-        console.log(`Iteration: ${iteration}`)
+        let results: {win: StateToReactions; loose: StateToReactions}; 
 
-        if (iteration === 200) {
-            await ausDieMausAI.train();
-        }
+        console.log(`Iteration: ${iteration}`);
 
-        if (iteration >= 200) {
-            winData = await newGame.training((state: AusDieMausState) => ausDieMausAI.classify(state));
-        } else {
-            winData = await newGame.training();
-        }
+        results = await newGame.training((state: AusDieMausState) => ausDieMausStatistics.getData(state));
 
-        ausDieMausAI.addData(winData);
+        ausDieMausStatistics.addData(results.win, true);
+        ausDieMausStatistics.addData(results.loose, false);
 
         iteration++;
+
         loadTraining();
     };
     loadTraining();
